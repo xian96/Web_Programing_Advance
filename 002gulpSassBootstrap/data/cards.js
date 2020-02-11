736fa12834ee87116cbd71e71d94d59bcd16d9a0
xian96 <ilovestartrek2@gmail.com>
2020-02-10 23:05:48 -0500

    git lab2 topic

--- 002gulpSassBootstrap/data/cards.js
+++ 002gulpSassBootstrap/data/cards.js
@@ -1,62 +1,62 @@
 module.exports = all = [
     {
-        name: "scp-1",
-        image: "https://cdn.pixabay.com/photo/2016/11/22/23/08/adventure-1851092_1280.jpg",
-        description: "the very first",
-        detail: "detail of the current"
+        name: "Spider-Man",
+        image: "https://vignette.wikia.nocookie.net/marveldatabase/images/9/96/Friendly_Neighborhood_Spider-Man_Vol_2_8_Granov_Variant_Textless.jpg/revision/latest?cb=20190608001208",
+        description: "Spider-Man is a fictional superhero created by writer-editor Stan Lee and writer-artist Steve Ditko. ",
+        detail: "Spider-Man is a fictional superhero created by writer-editor Stan Lee and writer-artist Steve Ditko. He first appeared in the anthology comic book Amazing Fantasy #15 (August 1962) in the Silver Age of Comic Books. He appears in American comic books published by Marvel Comics, as well as in a number of movies, television shows, and video game adaptations set in the Marvel Universe. In the stories, Spider-Man is the alias of Peter Parker, an orphan raised by his Aunt May and Uncle Ben in New York City after his parents Richard and Mary Parker were killed in a plane crash."
     },
     {
-        name: "scp-2",
-        image: "https://i.ya-webdesign.com/images/sample-text-png-13.png",
-        description: "the very 2",
-        detail: "SCP-002 resembles a tumorous, fleshy growth with a volume of roughly 60 m³ (or 2000 ft³). An iron valve hatch on one side leads to its interior, which appears to be a standard low-rent apartment of modest size. One wall of the room possesses a single window, though no such opening is visible from the exterior. The room contains furniture which, upon close examination, appears to be sculpted bone, woven hair, and various other biological substances produced by the human body. All matter tested thus far show independent or fragmented DNA sequences for each object in the room."
+        name: "Iron Man",
+        image: "https://vignette.wikia.nocookie.net/marveldatabase/images/9/9a/Iron_Man_2020_Vol_2_2_Textless.jpg/revision/latest?cb=20200116200342",
+        description: "Iron Man is a fictional superhero appearing in American comic books published by Marvel Comics.",
+        detail: "The character was co-created by writer and editor Stan Lee, developed by scripter Larry Lieber, and designed by artists Don Heck and Jack Kirby. The character made his first appearance in Tales of Suspense #39 (cover dated March 1963), and received his own title in Iron Man #1 (May 1968)."
     },
     {
-        name: "scp-3",
-        image: "https://i.ya-webdesign.com/images/sample-text-png-13.png",
-        description: "the very 3",
-        detail: "detail of the current"
+        name: "Captain America",
+        image: "https://vignette.wikia.nocookie.net/marveldatabase/images/b/b1/Captain_America_Vol_6_2_Textless.jpg/revision/latest?cb=20110719042719",
+        description: "Captain America is a fictional superhero appearing in American comic books published by Marvel Comics. ",
+        detail: "Created by cartoonists Joe Simon and Jack Kirby, the character first appeared in Captain America Comics #1 (cover dated March 1941) from Timely Comics, a predecessor of Marvel Comics. Captain America was designed as a patriotic supersoldier who often fought the Axis powers of World War II and was Timely Comics' most popular character during the wartime period. The popularity of superheroes waned following the war, and the Captain America comic book was discontinued in 1950, with a short-lived revival in 1953. Since Marvel Comics revived the character in 1964, Captain America has remained in publication."
     },
     {
-        name: "scp-4",
-        image: "https://i.ya-webdesign.com/images/sample-text-png-13.png",
-        description: "the very 4",
-        detail: "detail of the current"
+        name: "Deadpool",
+        image: "https://vignette.wikia.nocookie.net/marveldatabase/images/c/c4/Deadpool_Vol_7_1_Textless.jpg/revision/latest?cb=20180601075902",
+        description: "Deadpool (Wade Winston Wilson) is a fictional character appearing in American comic books published by Marvel Comics.",
+        detail: "Created by writer Fabian Nicieza and artist/writer Rob Liefeld, the character first appeared in The New Mutants #98 (cover-dated February 1991). Initially Deadpool was depicted as a supervillain when he made his first appearance in The New Mutants and later in issues of X-Force, but later evolved into his more recognizable antiheroic persona. Deadpool, whose real name is Wade Wilson, is a disfigured mercenary with the superhuman ability of an accelerated healing factor and physical prowess."
     },
     {
-        name: "scp-5",
-        image: "https://i.ya-webdesign.com/images/sample-text-png-13.png",
-        description: "the very 5",
-        detail: "detail of the current"
+        name: "Hulk",
+        image: "https://vignette.wikia.nocookie.net/marveldatabase/images/d/dc/Immortal_Hulk_20_Unknown_Comic_Books_Exclusive_Virgin_Variant.jpg/revision/latest?cb=20190830071143",
+        description: "The Hulk is a fictional superhero appearing in publications by the American publisher Marvel Comics. ",
+        detail: "Created by writer Stan Lee and artist Jack Kirby, the character first appeared in the debut issue of The Incredible Hulk (May 1962). In his comic book appearances, the character is both the Hulk, a green-skinned, hulking and muscular humanoid possessing a vast degree of physical strength, and his alter ego Dr. Robert Bruce Banner, a physically weak, socially withdrawn, and emotionally reserved physicist, the two existing as independent personalities and resenting of the other."
     },
     {
-        name: "scp-6",
-        image: "https://i.ya-webdesign.com/images/sample-text-png-13.png",
-        description: "the very 6",
-        detail: "detail of the current"
+        name: "Captain Marvel",
+        image: "https://vignette.wikia.nocookie.net/marveldatabase/images/0/0a/Carol_Danvers_%28Earth-616%29_from_Life_of_Captain_Marvel_Vol_2_4_Bartel_Variant_cover.jpg/revision/latest?cb=20180726214728",
+        description: "Captain Marvel is the name of several fictional superheroes appearing in American comic books published by Marvel Comics. ",
+        detail: "My name is Captain Marvel... I am an Earthling and an Avenger. But today I stand as one with the settlers of Torfa, who claim this planet and its resources as they were freely given in the aftermath of the Behemoth disaster... They are a peaceful people, but I am a woman of war. If you move against them, you move against me. I am willing to die here today, for this cause. I have made my choice... Now you make yours"
     },
     {
-        name: "scp-7",
-        image: "https://i.ya-webdesign.com/images/sample-text-png-13.png",
-        description: "the very 7",
-        detail: "detail of the current"
+        name: "Thanos",
+        image: "https://vignette.wikia.nocookie.net/marveldatabase/images/c/cb/Infinity_Vol_1_4_Generals_Variant_Textless.jpg/revision/latest?cb=20130628193007",
+        description: "Thanos is a fictional supervillain appearing in American comic books published by Marvel Comics.",
+        detail: "Am I not Thanos?! Did I not butcher the woman who gave me birth, who forced me into this hell called life? Is not the wake of my passing crimson with the blood of my enemies and allies alike?! Death is with me every second of the day!"
     },
     {
-        name: "scp-8",
-        image: "https://i.ya-webdesign.com/images/sample-text-png-13.png",
-        description: "the very 8",
-        detail: "detail of the current"
+        name: "Guardians of the Galaxy",
+        image: "https://vignette.wikia.nocookie.net/marveldatabase/images/9/9b/Guardians_of_the_Galaxy_%28Earth-616%29_from_Guardians_of_the_Galaxy_Vol_6_1_Lim_Variant_001.jpg/revision/latest?cb=20200210014315",
+        description: "The Guardians of the Galaxy is a fictional superhero team appearing in American comic books published by Marvel Comics.",
+        detail: "You can call the Guardians whatever you want to. But to the rest of the galaxy, all the people we've helped crawl out from under you and crap like you... the Guardians are the good guys. Not to brag, but we actually go out of our way to help people. We help people who can't help themselves."
     },
     {
-        name: "scp-9",
-        image: "https://i.ya-webdesign.com/images/sample-text-png-13.png",
-        description: "the very 9",
-        detail: "detail of the current"
+        name: "X-Men",
+        image: "https://vignette.wikia.nocookie.net/marveldatabase/images/e/eb/House_of_X_Vol_1_3_Textless.jpg/revision/latest?cb=20190523185423",
+        description: "X-Men are a team of fictional mutant superheroes appearing in American comic books published by Marvel Comics.",
+        detail: "The X-Men are a privately owned security force created to harbor peace between the emerging homo superior, nicknamed Mutants and baseline humans. The roster is made up of Mutants who have been trained and educated in the use of their powers at the Xavier's School for Gifted Youngsters.[6] Their operations are funded by Professor Charles Xavier's family fortune and from the patents he created"
     },
     {
-        name: "scp-10",
-        image: "https://i.ya-webdesign.com/images/sample-text-png-13.png",
-        description: "the very last",
-        detail: "detail of the current"
+        name: "Magneto",
+        image: "https://vignette.wikia.nocookie.net/marveldatabase/images/e/e1/Max_Eisenhardt_%28Earth-616%29_from_House_of_X_Vol_1_1_001_%281%29.png/revision/latest?cb=20190724212749",
+        description: "A magneto is an electrical generator that uses permanent magnets to produce periodic pulses of alternating current.",
+        detail: "It's a powerful mutant with the ability to generate and control magnetic fields. Magneto regards mutants as evolutionary superior to humans and rejects the possibility of peaceful human-mutant coexistence; he aims to conquer the world to enable mutants, whom he refers to as Homo superior, to replace humans as the dominant species"
     }
 ];
